2479194bd46c43bb54e8dc8ca125beaf43865366
aahsnr <ahsanur041@proton.me>
2025-09-08 08:32:57 +0600

    updated

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.ai_cmp = false
-
+vim.g.snacks_animate = false
 local opt = vim.opt
 opt.relativenumber = false
 opt.wrap = true
