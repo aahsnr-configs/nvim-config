182e4ed93ab7fc47f280a26745af3c0dd9424dc1
aahsnr-configs <ahsanur041@proton.me>
2025-09-15 17:21:28 -0400

    updated

--- lua/community.lua
+++ lua/community.lua
@@ -3,7 +3,19 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.nix" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.hyprlang" },
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.nix" },
+  { import = "astrocommunity.pack.nvchad-ui" },
+  { import = "astrocommunity.pack.prettier" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.scrolling.neoscroll-nvim" },
+  { import = "astrocommunity.fuzzy-finder.telescope-nvim" },
+  { import = "astrocommunity.fuzzy-finder.telescope-zoxide" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
+  { import = "astrocommunity.markdown-and-latex.texpresso-vim" },
+  { import = "astrocommunity.snippet.nvim-snippets" },
 }
